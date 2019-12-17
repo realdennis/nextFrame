3a1924fa910c0bc003bd0dc0c2250e422055450e
realdennis <realdennis777@gmail.com>
2019-12-17 23:53:51 +0800

    nextFrameFactory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 const isFunction = fn => typeof fn === "function";
 
-export const nextFrame = async () =>
-  new Promise(resolve => window.requestAnimationFrame(resolve));
+export const nextFrameFactory = () => {
+  let currentFrame = 0;
+  let cancel;
+  const signal = new Promise((_, reject) => {
+    cancel = () => reject();
+  });
+  const next = () =>
+    new Promise((resolve, reject) => {
+      const rafID = window.requestAnimationFrame(() => {
+        currentFrame++;
+        resolve({ message: "Arrived", currentFrame });
+      });
+      signal.catch(() => {
+        // clean up the effect and reject this `next` promise
+        window.cancelAnimationFrame(rafID);
+        reject({
+          message: "canceled",
+          currentFrame
+        });
+      });
+    });
+  return { cancelNextFrame: cancel, nextFrame: next };
+};
+
+export const { nextFrame } = nextFrameFactory();
 
 export const perFrameReducer = async (_prev, next) => {
   await nextFrame();
